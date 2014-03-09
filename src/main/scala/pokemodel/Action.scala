1bbcdf318554c1a26405dc29000a72950e789d77
hoxiea <hoxiea@gmail.com>
2014-03-09 17:43:45 -0400

    Experimenting with Actions as case classes

--- src/main/scala/pokemodel/Action.scala
+++ src/main/scala/pokemodel/Action.scala
@@ -1,5 +1,11 @@
 package pokemodel
 
-class Action {
+// TODO: Possible actions are useMove1, useMove2, useMove3, useMove4, or switch
+// TODO: But these need to account for the multi-turn and out-of-control things too
 
-}
+sealed abstract class Action
+case object Switch extends Action
+case object useMove1 extends Action
+case object useMove2 extends Action
+case object useMove3 extends Action
+case object useMove4 extends Action
