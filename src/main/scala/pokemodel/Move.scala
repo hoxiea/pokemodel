62038395d9fc8ac636c4086e59e7aa613c07901c
hoxiea <hoxiea@gmail.com>
2014-03-09 17:44:19 -0400

    Still a mess... commented out Pound to make it compile

--- src/main/scala/pokemodel/Move.scala
+++ src/main/scala/pokemodel/Move.scala
@@ -1,15 +1,24 @@
 package pokemodel
 
-abstract class Move {
-  val p : Option[Pokemon]    // a Move instance has either been assigned to one Pokemon, or not 
-  val accuracy : Float       // in [0.0, 1.0]
-  val critHitRate : Float    // in [0.0, 1.0]
+import scala.collection.mutable
+
+// TODO: Add the fact that only centain Pokemon can learn certain moves
+// Can be scraped from pages such as 
+// http://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)/Generation_I_learnset
+
+abstract class Move { 
+  val accuracy : Double       // in [0.0, 1.0]
+  val critHitRate : Double    // in [0.0, 1.0]
   val type1 : Type.Value
+  // val action : Action
+  val power : Int
   
   val maxPP : Int
   var currentPP : Int
   
-  val action : Action
+  def use(pb : Battle) = {
+    // TODO: do things like
+  }
   
   def restorePP(amount : Int) = {
     currentPP = maxPP.min(currentPP + amount)
@@ -20,4 +29,10 @@ abstract class Move {
   } 
 }
 
-class Pound 
+//class Pound extends Move {
+//  val accuracy = 1.0
+//  val critHitRate = 1.0
+//  val type1 = Normal
+//  val maxPP = 35
+//  val power = 40
+//}
