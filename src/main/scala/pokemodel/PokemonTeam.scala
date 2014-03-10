aca9bd00ee8ee16135c69f99d74db8e23d1e59e6
hoxiea <hoxiea@gmail.com>
2014-03-10 12:35:14 -0400

    Slight change in implementation of Team

--- src/main/scala/pokemodel/PokemonTeam.scala
+++ src/main/scala/pokemodel/PokemonTeam.scala
@@ -2,15 +2,17 @@ package pokemodel
 
 class PokemonTeam(team: List[Pokemon]) {
   require(1 <= team.length && team.length <= 6, "Your team must have 1-6 Pokemon on it!")
-  
-  var activePokemon : Option[Pokemon]= team.find(_.currentHP > 0)
+  var activeIndex : Int = team.indexWhere(_.currentHP > 0)   // in 0 .. 5
+  def activePokemon : Pokemon = team(activeIndex)
   
   def healAll() {
     team.map(_.heal())
   }
   
-  def switch(index : Int) = {
-  
+  def switch(newIndex : Int) = {
+    require(0 <= newIndex && newIndex <= 5, s"newIndex $newIndex out of range for switch")
+    require(newIndex != activeIndex, s"tried to switch the active Pokemon in")
+    activeIndex = newIndex
   }
   
   def hasSomeoneAlive : Boolean = team.exists(_.currentHP > 0)
