cb563fead51b8115ef96d7af215482fc0156ec08
hoxiea <hoxiea@gmail.com>
2014-03-13 16:13:11 -0400

    fix typo

--- src/main/scala/pokemodel/StatusAilment.scala
+++ src/main/scala/pokemodel/StatusAilment.scala
@@ -1,13 +1,12 @@
 package pokemodel
 
 object StatusAilment extends Enumeration {
-  type Type = Value
+  type StatusAilment = Value
   val SLP = Value("SLP")
   val PSN = Value("PSN")
   val BRN = Value("BRN")
   val FRZ = Value("FRZ")
   val PAR = Value("PAR")
-
   
   val stringToStatusAilment: Map[String, StatusAilment.Value] = Map(
     "SLP" -> SLP,
