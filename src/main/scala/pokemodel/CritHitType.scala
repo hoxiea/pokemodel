d2cf854e8bdfbed9c4bdcee397951d2ead4ba020
hoxiea <hoxiea@gmail.com>
2014-03-31 13:00:04 -0400

    Add NEVER and ALWAYS CritHitType rates, so that in tests I can never or always land a critical hit

--- src/main/scala/pokemodel/CritHitType.scala
+++ src/main/scala/pokemodel/CritHitType.scala
@@ -2,6 +2,8 @@ package pokemodel
 
 object CritHitType extends Enumeration {
   type CritHitType = Value
-  val LOW = Value("LOW")
-  val HIGH = Value("HIGH")
+  val NEVER = Value("NEVER")   // for testing, 0% chance of Crit Hit
+  val LOW = Value("LOW")       // the standard, 512 denominator
+  val HIGH = Value("HIGH")     // the rare 64 denominator
+  val ALWAYS = Value("ALWAYS") // for testing, 100% chance of Crit Hit
 }
