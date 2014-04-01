4d430866146afc8e988523fca07b03b7bd7acca9
hoxiea <hoxiea@gmail.com>
2014-03-31 20:30:25 -0400

    Way to keep track of moves that have happened... needed it for Counter

---
+++ src/main/scala/pokemodel/MoveHistory.scala
@@ -0,0 +1,10 @@
+package pokemodel
+
+import scala.collection.mutable
+
+class MoveHistory {
+  private val moveResults: mutable.Stack[MoveResult] = mutable.Stack[MoveResult]()
+  
+  def mostRecent: MoveResult = moveResults.top
+  def addToHistory(mr: MoveResult) { moveResults.push(mr) }
+}
