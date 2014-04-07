ad5cef5207923fe18c6643933be9cc7c73978da7
hoxiea <hoxiea@gmail.com>
2014-04-07 12:27:20 -0400

    Various classes of weird moves are going to use traits and distinguishing enums to register their behavior with the correct data structure

---
+++ src/main/scala/pokemodel/WeirdMoveEnums.scala
@@ -0,0 +1,8 @@
+package pokemodel
+
+object ViolentStruggleType extends Enumeration {
+  type ViolentStruggleType = Value
+  val THRASH = Value("THRASH")
+  val PETALDANCE = Value("PETALDANCE")
+}
+
